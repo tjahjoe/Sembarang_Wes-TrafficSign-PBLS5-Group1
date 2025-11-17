0eacde8f21c71b0f61f8be6715e6d2ee00475a4e
tjahjoe <ntell634@gmail.com>
2025-11-17 07:26:02 +0700

    feat: implement YOLO image processing and prediction endpoint

--- server/app.py
+++ server/app.py
@@ -4,9 +4,12 @@ import joblib
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from ultralytics import YOLO
+from PIL import Image 
 from scipy.signal import convolve2d
 from skimage.color import rgb2gray
 from skimage.feature import hog
+import io
 import warnings
 
 app = Flask(__name__)
@@ -15,6 +18,7 @@ warnings.filterwarnings('ignore')
 
 SVM_MODEL_PATH = '../model/svm_traffic_sign_model.pkl'
 RF_MODEL_PATH = '../model/rf_traffic_sign_model.pkl'
+YOLO_MODEL_PATh = '../model/traffic_sign.pt'
 LABEL_PATH = '../pkg/list_label.txt'
 IMG_SIZE = (64, 64)
 
@@ -23,7 +27,8 @@ try:
     print(f"* Model '{SVM_MODEL_PATH}' berhasil dimuat.")
     rf_model = joblib.load(RF_MODEL_PATH)
     print(f"* Model '{RF_MODEL_PATH}' berhasil dimuat.")
-    
+    yolo_model = YOLO(YOLO_MODEL_PATh)
+    print(f"* Model '{YOLO_MODEL_PATh}' berhasil dimuat.")
     with open(LABEL_PATH, 'r') as f:
         list_label = [line.strip() for line in f.readlines()]
     print(f"* {len(list_label)} label berhasil dimuat.")
@@ -33,11 +38,13 @@ except FileNotFoundError as e:
     print("Pastikan 'svm_traffic_sign_model.pkl', 'rf_traffic_sign_model.pkl' dan 'list_label.txt' ada di folder yang sama.")
     svm_model = None
     rf_model = None
+    yolo_model = None
     list_label = []
 except Exception as e:
     print(f"Error saat memuat model atau label: {e}")
     svm_model = None
     rf_model = None
+    yolo_model = None
     list_label = []
 
 # def get_gradients(gray_image):
@@ -245,6 +252,50 @@ def predict_rf():
 
     except Exception as e:
         return jsonify({'error': f'Terjadi kesalahan: {str(e)}'}), 500
+
+
+@app.route('/predict-yolo', methods=['POST'])
+def predict():    
+    if yolo_model is None:
+        return jsonify({"error": "Model tidak berhasil dimuat"}), 500
+
+    if 'image' not in request.files:
+        return jsonify({"error": "Tidak ada file 'image' dalam request"}), 400
+
+    file = request.files['image']
+
+    if file.filename == '':
+        return jsonify({"error": "Tidak ada file yang dipilih"}), 400
+
+    if file:
+        try:
+            image_bytes = file.read()
+            pil_image = Image.open(io.BytesIO(image_bytes))
+
+            results = yolo_model.predict(source=pil_image, conf=0.5)
+
+            output_data = []
+            
+            result = results[0] 
+            
+            for box in result.boxes:
+                xyxy = box.xyxy[0].tolist() 
+                conf = box.conf[0].item()   
+                cls_id = int(box.cls[0].item()) 
+                
+                cls_name = yolo_model.names[cls_id] 
+                
+                output_data.append({
+                    "class_name": cls_name,
+                    "class_id": cls_id,
+                    "confidence": conf,
+                    "box_xyxy": xyxy
+                })
+
+            return jsonify(output_data)
+
+        except Exception as e:
+            return jsonify({"error": f"Error saat memproses gambar: {e}"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
